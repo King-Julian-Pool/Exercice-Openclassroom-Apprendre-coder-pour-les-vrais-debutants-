4b32367f4c00b607cd4edaeeb11633714b296108
clement <clementlessieur@hotmail.fr>
2021-03-11 23:57:11 +0100

    solution pour l'exercice

---
+++ GettingStarted/NombreMystereConsole/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace NombreMystereConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            NouvellePartie();
+
+            YouWin();
+        }
+        static void NouvellePartie ()
+        {
+            int NumMys = GenereNombreAleatoire();
+
+            Console.WriteLine("Quel est le numéro mystère ?");
+
+            int PropNum = ChoisirUnNombre();
+
+
+            while (PropNum != NumMys)
+            {
+                PropNum = TryAgain(PropNum, NumMys);
+            }
+        }
+        static int GenereNombreAleatoire ()
+        {
+            return new Random().Next(1, 20);
+        }
+        static int ChoisirUnNombre()
+        {
+            string Prop = Console.ReadLine();
+            int PropNum;
+
+            while (int.TryParse(Prop, out PropNum) == false)
+            {
+                Console.WriteLine("Ceci n'est pas un nombre. Faites un petit effort!");
+                Prop = Console.ReadLine();
+            }
+            return PropNum;
+        }
+        static int TryAgain (int PropNum, int NumMys)
+        {
+            if (PropNum < NumMys)
+            {
+                Console.WriteLine("c'est plus");
+            }
+            else if (PropNum > NumMys)
+            {
+                Console.WriteLine("c'est moins");
+            }
+
+            PropNum = ChoisirUnNombre();
+            return PropNum;
+        }
+        static void YouWin ()
+        {
+            Console.WriteLine("Bravo tu as gagné !");
+        }
+
+    }
+}
---
+++ GettingStarted/SayMyName/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SayMyName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who are you ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " !");
+            Console.WriteLine("When were you born ?");
+            string year = Console.ReadLine();
+            int yearnum = int.Parse(year);
+            int age = DateTime.Now.Year - yearnum;
+            Console.WriteLine("You are " + age.ToString() + " years old");
+            if (age < 40)
+            {
+                Console.WriteLine("Tu es un BG");
+            }
+            else
+            {
+                Console.WriteLine("Tu est un vieux BG");
+            }
+        }
+    }
+}
